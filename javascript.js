3804e0086cf7c443ba081f8ddf00a32d98b0b126
Maikel Dekker <99040039@mydavinci.nl>
2018-09-05 14:45:14 +0200

    Style added & StartQuiz, ResetQuiz, PreviousQuestion functions added

--- javascript.js
+++ javascript.js
@@ -5,17 +5,33 @@ var proParties = document.getElementById("proParties");
 var ambivalentParties = document.getElementById("ambivalentParties");
 var contraParties = document.getElementById("contraParties");
 
-document.getElementById("startButton").onclick = function () {
-    document.getElementById("question").style.display = "block";
-    document.getElementById("startButton").style.display = "none";
-    question = 0;
-    LoadQuestion();
-}
+document.getElementById("startButton").onclick = function () { StartQuiz() };
 document.getElementById("partiesButton").onclick = function () { LoadParties() };
 document.getElementById("agree").onclick = function () { NextQuestion("agree") };
 document.getElementById("neither").onclick = function () { NextQuestion("neither") };
 document.getElementById("disagree").onclick = function () { NextQuestion("disagree") };
+document.getElementById("skip").onclick = function () { NextQuestion("skip") };
+document.getElementById("backButton").onclick = function () { PreviousQuestion() };
 
+function StartQuiz()
+{
+    document.getElementById("question").style.display = "block";
+    document.getElementById("backButton").style.display = "block";
+    document.getElementById("startButton").style.display = "none";
+    document.getElementById("logo").style.display = "none";
+    question = 0;
+    LoadQuestion();
+}
+function ResetQuiz()
+{
+    document.getElementById("results").style.display = "none";
+    document.getElementById("question").style.display = "none";
+    document.getElementById("backButton").style.display = "none";
+    document.getElementById("startButton").style.display = "block";
+    document.getElementById("logo").style.display = "block";
+    question = 0;
+    result = [];
+}
 function NextQuestion(string) {
     partiesCreated = false;
     if (string == "agree") {
@@ -24,14 +40,32 @@ function NextQuestion(string) {
         result.push("ambivalent");
     } else if (string == "disagree") {
         result.push("contra");
+    } else if (string == "skip") {
+        result.push("skip");
     }
     question++;
     if (question < subjects.length) {
-        document.getElementById("partiesList").style.display = "none";
         ClearParties();
         LoadQuestion();
     } else {
         GetResults();
+    }
+}
+function PreviousQuestion()
+{
+    partiesCreated = false;
+    if(question < 1)
+    {
+        ResetQuiz();
+    }else if(question >= subjects.length)
+    {
+        ResetQuiz();
+    }else
+    {
+        result.pop();
+        question = question - 1;
+        ClearParties();
+        LoadQuestion();
     }
 }
 function LoadQuestion() {
@@ -48,7 +82,11 @@ function LoadParties() {
             partyId = partyId.replace(/ /g, "");
             partyId = partyId.replace("\"\"", "/");
             var party = document.createElement('div');
-            party.innerHTML = "<label id=" + partyId + ">" + parties[counter].name + "</label>";
+            party.id = partyId;
+            party.style.borderBottom = "thin solid #000000";
+            var partyTitle = document.createElement('h3');
+            partyTitle.innerHTML = parties[counter].name;
+            party.appendChild(partyTitle);            
 
             if (parties[counter].position == "pro") {
                 proParties.appendChild(party);
@@ -71,6 +109,7 @@ function LoadParties() {
     }
 }
 function ClearParties() {
+    document.getElementById("partiesList").style.display = "none";
     while (proParties.lastChild) {
         proParties.removeChild(proParties.lastChild)
     }
@@ -84,10 +123,8 @@ function ClearParties() {
 function GetResults() {
     document.getElementById("question").style.display = "none";
     var results = document.getElementById("results");
-    results.style.display = "block";
-    results.innerHTML = result;
+    results.style.display = "flex";
     var subjectsCounter = 0;
-    var partiesResults = [];
     var partyObject = new Object();
     var counter = 0
     for (i in subjects[subjectsCounter].parties) {
@@ -95,29 +132,30 @@ function GetResults() {
         matchName = matchName.replace(/ /g, "");
         matchName = matchName.replace("\"\"", "/");
         partyObject[matchName] = 0;
+        var partyMatches = document.createElement("label");
+        var varName = [matchName] + "Element";
+        partyMatches.id = [varName]
+        results.appendChild(partyMatches);
         counter++;
     }
     for (i in subjects) {
         var partiesCounter = 0;
         for (i in subjects[subjectsCounter].parties) {
-            var parties = subjects[subjectsCounter].parties;
-            var position = parties[partiesCounter].position;
-            var matchName = subjects[subjectsCounter].parties[partiesCounter].name + "Matched";
+            var party = subjects[subjectsCounter].parties[partiesCounter];
+            var position = party.position;
+            var matchName = party.name + "Matched";
             matchName = matchName.replace(/ /g, "");
             matchName = matchName.replace("\"\"", "/");
 
             if (position == result[subjectsCounter]) {
-                partyObject[matchName] = partyObject[matchName] + 1;                
-                alert(partyObject[matchName]);
+                partyObject[matchName] = partyObject[matchName] + 1;
+                var partyElement = document.getElementById([matchName] + "Element");
+                partyElement.innerHTML = party.name + " = " + partyObject[matchName];
             } else {
 
             }
             partiesCounter++;
         }
         subjectsCounter++;
-    }
-    SortResults();
-    function SortResults() {
-        
     }
 }
